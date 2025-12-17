33c8b9a11aa8fff7df53c1cc30a05a5d27cb6022
afbrbza <afbrbza@unicamp.br>
2025-12-17 16:10:39 -0300

    :recycle: refactor(install.sh): Adding OpenBlas reference MUMPS install

--- install.mumps.sh
+++ install.mumps.sh
@@ -4,7 +4,14 @@ cd mumps/
 sudo rm -rf /opt/mumps/*
 # ls -la /opt/mumps/
 rm -rf build
-cmake -Bbuild -DBUILD_DOUBLE=on -DMUMPS_parallel=false -DMUMPS_openmp=on -DBUILD_SHARED_LIBS=on --install-prefix /opt/mumps
+sudo apt install -y libopenblas-dev liblapack-dev
+cmake -Bbuild \
+    -DBUILD_DOUBLE=on \
+    -DMUMPS_parallel=false \
+    -DMUMPS_openmp=on \
+    -DBUILD_SHARED_LIBS=on \
+    -DLAPACK_VENDOR=OpenBLAS \
+    --install-prefix /opt/mumps
 sudo cmake --build build
 sudo cmake --install build
 # cmake -S example -B example/build -DMUMPS_ROOT=build/local
